3509b7b6f71cab52df590f282c5eb5d5c57806f0
James Tombleson <luther38@gmail.com>
2022-03-01 16:01:52 -0800

    Updated tests

--- test/Import-PowerManagerManifest.Tests.ps1
+++ test/Import-PowerManagerManifest.Tests.ps1
@@ -1,22 +1,33 @@
-BeforeAll {
-
-}
-
 Describe "Import-PowerManagerManifest" {
     It "Should error if the file is missing" {
-
+        $manifestPath = Get-PowerManagerConfig -Manifest
+        $manifestExists = Test-Path $manifestPath
+        if ($manifestExists -eq $true ) { Remove-Item $manifestPath }
+        { Import-PowerManagerManifest } | Should -Throw
     }
 
     It "Should error if the file exists but is empty" {
-
+        $manifestPath = Get-PowerManagerConfig -Manifest
+        $manifestExists = Test-Path $manifestPath
+        if ($manifestExists -eq $true ) { Remove-Item $manifestPath }
+        New-Item $manifestPath
+        { Import-PowerManagerManifest } | Should -Throw
+        Remove-Item $manifestPath -Force
     }
 
     It "Should return a hashtable if the file exists and has proper format" {
-
+        $manifestPath = Get-PowerManagerConfig -Manifest
+        $manifestExists = Test-Path $manifestPath
+        if ($manifestExists -eq $true ) { Remove-Item $manifestPath }
+        New-PowerManagerManifest
+        $res = Import-PowerManagerManifest 
+        $res | should -not -Be ""
+        #Remove-Item $manifestPath -Force
     }
 
     It "Should have the Project Key" {
-        
+        $res = Import-PowerManagerManifest
+        $res.Project | Should be $res.Project
     }
 
 }
--- test/Test-PowerManagerManifestFile.Tests.ps1
+++ test/Test-PowerManagerManifestFile.Tests.ps1
@@ -6,6 +6,9 @@ BeforeAll {
 Describe 'Test-PowerManagerManifestFile'{
     It "Should return false if the file is not present" {
         #$path = ".\.pmproject"
+        $manifestPath = Get-PowerManagerConfig -Manifest
+        $manifestExists = Test-Path $manifestPath
+        if ($manifestExists -eq $true ) { Remove-Item $manifestPath -Force }
         Test-PowerManagerManifestFile | Should be $false
     }
 
